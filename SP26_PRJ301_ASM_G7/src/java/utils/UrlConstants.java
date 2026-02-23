9822ec4f6eb048dced8d4e2e3461e2fa3f3b71b8
minhcamon <nhatminhyt06@gmail.com>
2026-02-23 21:53:33 +0700

    base: url constants

---
+++ SP26_PRJ301_ASM_G7/src/java/utils/UrlConstants.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author Admin
+ */
+public class UrlConstants {
+    public static final String URL_ADMIN = "/admin";
+    public static final String URL_MANAGER = "/manager";
+    public static final String URL_STAFF = "/admin";
+}
